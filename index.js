5056b220dcbb60e4a37b82d9f9a69932bf982af2
Ayşe Saflo <ayshe.997@gmail.com>
2021-05-04 11:29:47 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,2 +1,19 @@
 
 // Write your code here
+// Write your code here
+const num1 = 31;
+const num2 = 2;
+let multiply = (num1*num2);
+console.log(multiply);
+
+let randomNum = (Math.floor(Math.random() * 100) + 1);
+console.log(randomNum);
+
+let num3 = 22;
+let num4 = 6;
+let mod = num3%num4;
+console.log(mod);
+
+let values = [2 , 10 , 9 , 20 , 4];
+let max = Math.max(...values);
+console.log(max);
